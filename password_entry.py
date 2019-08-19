fdf70466579c1ab75a50b9d19c7f83453fe371b7
Anjitha <anjithaajith.kadampuzha@my.jcu.edu.au>
2019-08-19 22:35:58 +1000

    "password checker code"

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,6 @@
 """Anjitha Ajith"""
+
+password = input("Enter a password of length atleast 6 characters ")
+if len(password) < 6:
+    password = input("Enter a password of length atleast 6 characters ")
+print("*" * len(password))
